914510c2839d2bc0621d505bfe07b8857d614e44
Petrick <petrick91@hotmail.com>
2021-06-01 17:40:27 -0400

    Refactoração da classe

--- app/Models/Persistencia/persistTicket.php
+++ app/Models/Persistencia/persistTicket.php
@@ -2,8 +2,10 @@
 namespace App\Models\Persistencia;
 
 use App\Helpers\Datas;
+use App\Http\Soap\Cliente2;
 use App\Models\Entity\tbTickets;
 use App\Models\Entity\tbUsuarios;
+use App\Rotinas\RotinaNovosTickets\NovosTickets;
 use DateTime;
 use Doctrine\DBAL\Types\DateTimeType;
 use Doctrine\ORM\EntityManagerInterface;
@@ -17,53 +19,90 @@ class persistTicket
     private $data;
     private $dataHoje;
 
-    public function __construct(EntityManagerInterface $em)
+
+    private $codticket;
+    private $coddepartamento;
+    private $codcategoria;
+    private $codclass;
+    private $codstatus;
+    private $titulo;
+    private $codusuario;
+    private $prioridade;
+    private $avaliacao;
+    private $bloqueado;
+    private $dtabertura;
+    private $dtalteracao;
+    private $dtfechamento;
+    private $codcliente;
+    private $dtleituracliente;
+    private $dtprevisao;
+
+
+
+    public function __construct(EntityManagerInterface $em,$codticket ,$coddepartamento, $codcategoria, $codclass ,$codstatus, $titulo, $codusuario,$prioridade, $avaliacao, $bloqueado, $dtabertura, $dtalteracao, $dtfechamento, $codcliente, $dtleituracliente,$dtprevisao)
     {
         $this->em = $em;
         $this->platform = new SQLServer2012Platform();
         $this->data = new DateTimeType;
         $this->dataHoje = new DateTime('now');
+
+
+        $this->codticket = $codticket;
+        $this->coddepartamento = $coddepartamento;
+        $this->codcategoria = $codcategoria;
+        $this->codclass = $codclass;
+        $this->codstatus = $codstatus;
+        $this->titulo = $titulo;
+        $this->codusuario = $codusuario;
+        $this->prioridade = $prioridade;
+        $this->avaliacao = $avaliacao;
+        $this->bloqueado = $bloqueado;
+        $this->dtabertura = $dtabertura;
+        $this->dtalteracao = $dtalteracao;
+        $this->dtfechamento = $dtfechamento;
+        $this->codcliente = $codcliente;
+        $this->dtleituracliente = $dtleituracliente;
+        $this->dtprevisao = $dtprevisao;
+
+
     }
 
-    public function gravaTicketsbanco($codticket, $coddepartamento,$codcategoria,$codclass, $codstatus, $titulo, $codusuario,$prioridade, $avaliacao, $bloqueado, $dtabertura, $dtalteracao, $dtfechamento, $codcliente, $dtleituracliente,$dtprevisao)
+    public function gravaTicketsbanco()
     {
-        $ticket = $this->em->find(tbTickets::class, (int)$codticket);
+        $ticket = $this->em->find(tbTickets::class, (int)$this->codticket);
 
         if (!$ticket)
         {
+            //$novoTicket = new NovosTickets($this->em,(int)$this->codticket, $this->codcliente);
+            //$novoTicket->processaNovosTickets();
             $ticket = new tbTickets();
-            $ticket->setCodticket($codticket);
+            $ticket->setCodticket((int)$this->codticket);
             $this->em->flush();
-            /*             echo "Ticket--> {$codticket} INSERIDO <br />" ; */
             $this->em->persist($ticket);
-/*          echo "Ticket--> {$codticket} ATUALIZADO <br />" ; */
-            //$this->insertTickets($ticket,$coddepartamento,$codcategoria,$codclass, $codstatus, $titulo, $codusuario,$prioridade, $avaliacao,$bloqueado, $dtabertura, $dtalteracao, $dtfechamento, $codcliente, $dtleituracliente);
         }
 
-        $this->insertTickets($ticket,$coddepartamento,$codcategoria,$codclass, $codstatus, $titulo, $codusuario,$prioridade,  $avaliacao, $bloqueado, $dtabertura, $dtalteracao, $dtfechamento, $codcliente, $dtleituracliente,$dtprevisao);
+        $this->insertTickets($ticket);
 
 
     }
 
-    private function insertTickets($ticket,$coddepartamento,$codcategoria,$codclass, $codstatus, $titulo, $codusuario,$prioridade,  $avaliacao, $bloqueado, $dtabertura, $dtalteracao, $dtfechamento, $codcliente, $dtleituracliente,$dtprevisao)
+    private function insertTickets($ticket)
     {
-        $ticket->setCoddepartamento($coddepartamento);
-        $ticket->setCodcategoria($codcategoria);
-        $ticket->setCodclass($codclass);
-        $ticket->setCodstatus($codstatus);
-        $ticket->setTitulo($titulo);
-        $ticket->setCodusuario($this->em->find(tbUsuarios::class,(int)$codusuario));
-        //$ticket->setCodusuario($ticket->getCodusuario()->getCodusuario((int)$codusuario));
-        //$ticket->setCodusuario($this->em->getReference(tbUsuarios::class,(int)$codusuario) );
-        $ticket->setPrioridade($prioridade);
-        $ticket->setAvaliacao($avaliacao);
-        $ticket->setBloqueado($bloqueado);
-        $ticket->setDtabertura(Datas::converteDataIntEmDataBanco((int) $dtabertura, $this->platform)) ;
-        $ticket->setDtalteracao(Datas::converteDataIntEmDataBanco((int) $dtalteracao, $this->platform));
-        $ticket->setDtfechamento(Datas::converteDataIntEmDataBanco((int) $dtfechamento, $this->platform));
-        $ticket->setCodcliente($codcliente);
-        $ticket->setDtleituracliente(Datas::converteDataIntEmDataBanco((int) $dtleituracliente,$this->platform));
-        $ticket->setDtprevisao(Datas::converteDataIntEmDataBanco((int)$dtprevisao, $this->platform));
+        $ticket->setCoddepartamento($this->coddepartamento);
+        $ticket->setCodcategoria($this->codcategoria);
+        $ticket->setCodclass($this->codclass);
+        $ticket->setCodstatus($this->codstatus);
+        $ticket->setTitulo($this->titulo);
+        $ticket->setCodusuario($this->em->find(tbUsuarios::class,(int)$this->codusuario));
+        $ticket->setPrioridade($this->prioridade);
+        $ticket->setAvaliacao($this->avaliacao);
+        $ticket->setBloqueado($this->bloqueado);
+        $ticket->setDtabertura(Datas::converteDataIntEmDataBanco((int) $this->dtabertura, $this->platform)) ;
+        $ticket->setDtalteracao(Datas::converteDataIntEmDataBanco((int) $this->dtalteracao, $this->platform));
+        $ticket->setDtfechamento(Datas::converteDataIntEmDataBanco((int) $this->dtfechamento, $this->platform));
+        $ticket->setCodcliente($this->codcliente);
+        $ticket->setDtleituracliente(Datas::converteDataIntEmDataBanco((int) $this->dtleituracliente,$this->platform));
+        $ticket->setDtprevisao(Datas::converteDataIntEmDataBanco((int)$this->dtprevisao, $this->platform));
         $this->em->flush();
 
     }
