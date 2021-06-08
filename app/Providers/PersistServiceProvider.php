240e0bbe1aeaf9a4f15e8aa1a64888415a3f5455
Petrick <petrick91@hotmail.com>
2021-06-08 19:34:01 -0400

    Alterado o nome da classe de  persistTicket para insertUpdateTicket

--- app/Providers/PersistServiceProvider.php
+++ app/Providers/PersistServiceProvider.php
@@ -1,16 +1,16 @@
 <?php
 namespace App\Providers;
 
+use App\Models\Persistencia\insertUpdateTicket;
 use App\Models\Persistencia\persistParametros;
 use Illuminate\Support\ServiceProvider;
-use App\Models\Persistencia\persistTicket;
 use App\Models\Persistencia\persistTicketItem;
 
 
 class PersistServiceProvider extends ServiceProvider
 {
     public function register(){
-        $this->app->bind('App\Models\Persistencia\persistTicket', persistTicket::class);
+        $this->app->bind('App\Models\Persistencia\insertUpdateTicket', insertUpdateTicket::class);
         $this->app->bind('App\Models\Persistencia\persistTicketItem', persistTicketItem::class);
         $this->app->bind('App\Models\Persistencia\persistParametros', persistParametros::class);
 
